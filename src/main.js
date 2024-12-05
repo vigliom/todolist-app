8e5d58da02fc9453e7aba7936fdce65944b9fe9f
Jorge Pérez <jperez@consoft.es>
2024-12-05 17:01:59 +0100

    Update dependencies and modify main.js: remove webfontloader, add Material Design Icons, and adjust script commands in package.json

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
-
-loadFonts();
+import '@mdi/font/css/materialdesignicons.css';
 
 createApp(App).use(router).use(store).use(vuetify).mount("#app");
